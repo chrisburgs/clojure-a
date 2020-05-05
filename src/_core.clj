57a1a82b6536a093e4b16553a755830ff4292c9d
Christopher Burger <burger.christopher@hotmail.ca>
2020-05-04 22:14:57 -0700

    create 2 array.....

--- src/_core.clj
+++ src/_core.clj
@@ -1,9 +1,13 @@
 (ns _core)
+(defn reload
+	"Attempt to reload code"
+	[]
+	(use '_core :reload))
 
 (defn main []
-  (println "Main..."))
+	(println "Main..."))
 
-(defn reload
-  "Attempt to reload code"
-  []
-  (use '_core :reload))
+(defn thing []
+	(def a (repeat 3 (into [] [0 0 0])))
+	(println a)
+)
